506b89806b02682192e3fffb0781aa69b9571c6e
lordlabuckdas <55460753+lordlabuckdas@users.noreply.github.com>
2021-08-12 23:55:13 +0530

    complete min sparse table and add some other tree nodes

--- sparse_table.hpp
+++ sparse_table.hpp
@@ -1,22 +1,85 @@
 #include<iostream>
 // TODO: use custom vector
 #include<vector>
-#include<math>
 
 namespace stl {
   class min_sparse_table {
     int N, P;
+    int *log2;
     int **lookup;
+    int **idx;
   public:
     min_sparse_table(std::vector<int>);
-    query(int, int);
+    int query(int, int);
+    int cascading_query(int, int);
+    int query_index(int, int);
+    ~min_sparse_table();
   }
   
   min_sparse_table::min_sparse_table(std::vector<int> values) {
     this->N = values.size();
-    this->P = log2(this->N);
-    this->lookup = new int[this->N];
-    for(int i=0;i<this->N;i++)
-      lookup[i] = new int[this->P];
+    this->log2 = new int[this->N+1];
+    log2[0] = log2[1] = 0;
+    for(int i=2;i<=this->N;i++)
+      log2[i] = log2[i/2] + 1;
+    this->P = log2[this->N];
+    this->lookup = new int*[this->P+1];
+    this->idx = new int*[this->P+1];
+    for(int i=0;i<=this->P;i++) {
+      lookup[i] = new int[this->N];
+      idx[i] = new int[this->N];
+    }
+    for(int i=0;i<this->N;i++) {
+      lookup[0][i] = values[i];
+      idx[0][i] = i;
+    }
+    for(int i=1;i<=this->P;i++) {
+      for(int j=0;j+(1<<i)<=this->N;j++) {
+        int left = lookup[i-1][j];
+        int right = lookup[i-1][j+(1<<(i-1))];
+        lookup[i][j] = min(left, right);
+        if(left<=right)
+          it[i][j] = it[i-1][j];
+        else
+          it[i][j] = it[i-1][j+(1<<(i-1))];
+      }
+    }
+  }
+
+  int min_sparse_table::query(int l, int r) {
+    int len = r-l+1;
+    int p = this->log2[len];
+    int left = lookup[p][l];
+    int right = lookup[p][r-(1<<p)+1];
+    return min(left, right);
+  }
+
+  int min_sparse_table::cascading_query(int l, int r) {
+    int min_val = INT_MAX;
+    for(int p=log2[r-l+1];l<=r;p=log2[r-l+1]) {
+      min_val = min(min_val, lookup[p][l]);
+      l += (1<<p);
+    }
+    return min_val;
+  }
+
+  int min_sparse_table::query_index(int l, int r) {
+    int len = r-l+1;
+    int p = this->log2[len];
+    int left = lookup[p][l];
+    int right = lookup[p][r-(1<<p)+1];
+    if(left<=right)
+      return idx[p][l];
+    return idx[p][r-(1<<p)+1];
+  }
+
+  min_sparse_table::~min_sparse_table() {
+    delete[] this->log2;
+    for(int i=0;i<=this->P;i++) {
+      delete[] this->lookup[i];
+      delete[] this->idx[i];
+    }
+    delete[] this->lookup;
+    delete[] this->idx;
   }
 }
--- tree.hpp
+++ tree.hpp
@@ -3,139 +3,174 @@
 #include <stack>
 
 namespace stl {
-	template <typename T> class treenode {
-		public:
-			T val;
-			treenode *left, *right;
-			// treenode *parent;
-			treenode(T);
-			// treenode(T, treenode<T>*);
-	};
+  template <typename T> class treenode {
+    public:
+      T val;
+      treenode *left, *right;
+      treenode(T);
+  };
 
-	// template <typename T> treenode<T>::treenode(T x): val(x), left(NULL), right(NULL), parent(NULL) {};
+  template <typename T> treenode<T>::treenode(T x): val(x), left(NULL), right(NULL) {};
 
-	// template <typename T> treenode<T>::treenode(T x, treenode<T> *p): val(x), left(NULL), right(NULL), parent(p) {};
-	
-	template <typename T> treenode<T>::treenode(T x): val(x), left(NULL), right(NULL) {};
+  template <typename T> class treenodep {
+    public:
+      T val;
+      treenodep *left, *right, *parent;
+      treenodep(T);
+      treenodep(T, treenodep<T>*);
+  };
 
-	template <typename T> class tree {
-		protected:
-			treenode<T> *root;
-		public:
-			tree();
-			tree(T);
-			bool empty();
-			treenode<T>* get_root();
-			void inorder();
-			void inorder_rec();
-			void inorder_rec(treenode<T>*);
-			void preorder();
-			void preorder_rec();
-			void preorder_rec(treenode<T>*);
-			void postorder();
-			void postorder_rec();
-			void postorder_rec(treenode<T>*);
-	};
+  template <typename T> treenodep<T>::treenodep(T x): val(x), left(NULL), right(NULL), parent(NULL) {};
 
-	template <typename T> tree<T>::tree(): root(NULL) {}
+  template <typename T> treenodep<T>::treenodep(T x, treenodep<T> *p): val(x), left(NULL), right(NULL), parent(p) {};
 
-	template <typename T> tree<T>::tree(T x): root(new treenode(x)) {}
+  template <typename T> class narytreenode {
+    public:
+      T val;
+      std::list<narytreenode*> children;
+      narytreenode(T);
+      narytreenode(T, narytreenode*);
+      int num_children();
+      void add_child(narytreenode*);
+  };
 
-	template <typename T> bool tree<T>::empty() {
-		if(root)
-			return false;
-		return true;
-	}
+  template <typename T> narytreenode<T>::narytreenode(T x): val(x) {};
 
-	template <typename T> treenode<T>* tree<T>::get_root() {
-		return this->root;
-	}
+  template <typename T> narytreenode<T>::narytreenode(T x, narytreenode* child): val(x) {
+    this->children.push(child);
+  }
 
-	template <typename T> void tree<T>::inorder() {
-		treenode<T> *curr = root;
-		std::stack<treenode<T>*> st;
-		while(curr!=NULL || !st.empty()) {
-			if(curr!=NULL) {
-				st.push(curr);
-				curr = curr->left;
-			} else {
-				curr = st.top();
-				st.pop();
-				std::cout<<curr->val<<' ';
-				curr = curr->right;
-			}
-		}
-	}
+  int template<typename T> narytreenode<T>::num_children() {
+    return this->children.size();
+  }
 
-	template <typename T> void tree<T>::inorder_rec() {
-		inorder_rec(root);
-	}
+  void template<typename T> narytreenode<T>::add_child(narytreenode* new_child) {
+    this->children.push(new_child);
+  }
 
-	template <typename T> void tree<T>::inorder_rec(treenode<T> *ptr) {
-		if(ptr) {
-			inorder_rec(ptr->left);
-			std::cout<<ptr->val<<' ';
-			inorder_rec(ptr->right);
-		}
-	}
+  template<typename T> class narytreenodep: public narytreenode {
+    public:
+      narytreenode *parent;
+  }
 
-	template <typename T> void tree<T>::preorder() {
-		treenode<T> *curr = root;
-		std::stack<treenode<T>*> st;
-		st.push(curr);
-		while(!st.empty()) {
-			curr = st.top();
-			st.pop();
-			std::cout<<curr->val<<' ';
-			if(curr->right)
-				st.push(curr->right);
-			if(curr->left)
-				st.push(curr->left);
-		}
-	}
+  template <typename T> class tree {
+    protected:
+      treenode<T> *root;
+    public:
+      tree();
+      tree(T);
+      bool empty();
+      treenode<T>* get_root();
+      void inorder();
+      void inorder_rec();
+      void inorder_rec(treenode<T>*);
+      void preorder();
+      void preorder_rec();
+      void preorder_rec(treenode<T>*);
+      void postorder();
+      void postorder_rec();
+      void postorder_rec(treenode<T>*);
+  };
 
-	template <typename T> void tree<T>::preorder_rec() {
-		preorder_rec(root);
-	}
+  template <typename T> tree<T>::tree(): root(NULL) {}
 
-	template <typename T> void tree<T>::preorder_rec(treenode<T> *ptr) {
-		if(ptr) {
-			std::cout<<ptr->val<<' ';
-			preorder_rec(ptr->left);
-			preorder_rec(ptr->right);
-		}
-	}
+  template <typename T> tree<T>::tree(T x): root(new treenode(x)) {}
 
-	template <typename T> void tree<T>::postorder() {
-		treenode<T> *curr = root;
-		std::stack<treenode<T>*> temp;
-		std::stack<T> st;
-		temp.push(curr);
-		while(!temp.empty()) {
-			curr = temp.top();
-			temp.pop();
-			st.push(curr->val);
-			if(curr->left)
-				temp.push(curr->left);
-			if(curr->right)
-				temp.push(curr->right);
-		}
-		while(!st.empty()) {
-			std::cout<<st.top()<<' ';
-			st.pop();
-		}
-	}
+  template <typename T> bool tree<T>::empty() {
+    if(root)
+      return false;
+    return true;
+  }
 
-	template <typename T> void tree<T>::postorder_rec() {
-		postorder_rec(root);
-	}
+  template <typename T> treenode<T>* tree<T>::get_root() {
+    return this->root;
+  }
 
-	template <typename T> void tree<T>::postorder_rec(treenode<T> *ptr) {
-		if(ptr) {
-			postorder_rec(ptr->left);
-			postorder_rec(ptr->right);
-			std::cout<<ptr->val<<' ';
-		}
-	}
+  template <typename T> void tree<T>::inorder() {
+    treenode<T> *curr = root;
+    std::stack<treenode<T>*> st;
+    while(curr!=NULL || !st.empty()) {
+      if(curr!=NULL) {
+        st.push(curr);
+        curr = curr->left;
+      } else {
+        curr = st.top();
+        st.pop();
+        std::cout<<curr->val<<' ';
+        curr = curr->right;
+      }
+    }
+  }
+
+  template <typename T> void tree<T>::inorder_rec() {
+    inorder_rec(root);
+  }
+
+  template <typename T> void tree<T>::inorder_rec(treenode<T> *ptr) {
+    if(ptr) {
+      inorder_rec(ptr->left);
+      std::cout<<ptr->val<<' ';
+      inorder_rec(ptr->right);
+    }
+  }
+
+  template <typename T> void tree<T>::preorder() {
+    treenode<T> *curr = root;
+    std::stack<treenode<T>*> st;
+    st.push(curr);
+    while(!st.empty()) {
+      curr = st.top();
+      st.pop();
+      std::cout<<curr->val<<' ';
+      if(curr->right)
+        st.push(curr->right);
+      if(curr->left)
+        st.push(curr->left);
+    }
+  }
+
+  template <typename T> void tree<T>::preorder_rec() {
+    preorder_rec(root);
+  }
+
+  template <typename T> void tree<T>::preorder_rec(treenode<T> *ptr) {
+    if(ptr) {
+      std::cout<<ptr->val<<' ';
+      preorder_rec(ptr->left);
+      preorder_rec(ptr->right);
+    }
+  }
+
+  template <typename T> void tree<T>::postorder() {
+    treenode<T> *curr = root;
+    std::stack<treenode<T>*> temp;
+    std::stack<T> st;
+    temp.push(curr);
+    while(!temp.empty()) {
+      curr = temp.top();
+      temp.pop();
+      st.push(curr->val);
+      if(curr->left)
+        temp.push(curr->left);
+      if(curr->right)
+        temp.push(curr->right);
+    }
+    while(!st.empty()) {
+      std::cout<<st.top()<<' ';
+      st.pop();
+    }
+  }
+
+  template <typename T> void tree<T>::postorder_rec() {
+    postorder_rec(root);
+  }
+
+  template <typename T> void tree<T>::postorder_rec(treenode<T> *ptr) {
+    if(ptr) {
+      postorder_rec(ptr->left);
+      postorder_rec(ptr->right);
+      std::cout<<ptr->val<<' ';
+    }
+  }
 
 }
